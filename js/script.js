3c5b1522a2ceb35f48a993f24a132f45b2117943
Carla Stockalper <cstockalper@gmail.com>
2018-07-12 16:17:51 -0400

    svg animation

---
+++ js/script.js
@@ -0,0 +1,26 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+$('.cls-1').attr("fill","red");
+
+})(jQuery); // End of use strict
+
+
+//Code taken from line map animation
+//
+// var pathEls = document.querySelectorAll('path');
+// for (var i = 0; i < pathEls.length; i++) {
+//   var pathEl = pathEls[i];
+//   var offset = anime.setDashoffset(pathEl);
+//   pathEl.setAttribute('stroke-dashoffset', offset);
+//   anime({
+//     targets: pathEl,
+//     strokeDashoffset: [offset, 0],
+//     duration: anime.random(1000, 3000),
+//     delay: anime.random(0, 2000),
+//     loop: true,
+//     direction: 'alternate',
+//     easing: 'easeInOutSine',
+//     autoplay: true
+//   });
+// }
